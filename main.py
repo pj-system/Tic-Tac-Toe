1a2a9b23eae63e92b5ad0928153109c8917193e3
pj-system <118063741+pj-system@users.noreply.github.com>
2022-12-02 23:03:29 +0000

    Solved with Minimax algorithm
    Working salutation with a minimax algorithm.
    1 human player + 1 Algorithm player
    Algorithm goes first by default, change current player to 'X' on line 96 to swap starting order.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import random
+import math
 
 
 class Game_Board:
 
     def __init__(self):
         self.board_dict = {
+
             7: ' ', 8: ' ', 9: ' ',
             4: ' ', 5: ' ', 6: ' ',
             1: ' ', 2: ' ', 3: ' ',
         }
 
+        self.win_options = [
+            [7, 8, 9], [4, 5, 6], [1, 2, 3],
+            [7, 4, 1], [8, 5, 2], [9, 6, 3],
+            [1, 5, 9], [7, 5, 3]
+        ]
+
+    # draws the board state
     def Update(self):
 
         print(
@@ -18,24 +27,40 @@ class Game_Board:
             + f'{self.board_dict[1]} | {self.board_dict[2]} | {self.board_dict[3]}'
         )
 
+    # check for any win
     def Check_For_Win(self):
 
-        # Horizontals
-        if self.board_dict[1] == self.board_dict[2] == self.board_dict[3] != ' ' or self.board_dict[4] == self.board_dict[5] == self.board_dict[6] != ' ' or self.board_dict[7] == self.board_dict[8] == self.board_dict[9] != ' ':
-            return True
-        # Verticals
-        elif self.board_dict[1] == self.board_dict[4] == self.board_dict[7] != ' ' or self.board_dict[2] == self.board_dict[5] == self.board_dict[8] != ' ' or self.board_dict[3] == self.board_dict[6] == self.board_dict[9] != ' ':
-            return True
-        # Cross
-        elif self.board_dict[7] == self.board_dict[5] == self.board_dict[3] != ' ' or self.board_dict[1] == self.board_dict[5] == self.board_dict[9] != ' ':
-            return True
-        else:
-            return False
+        win = False
+
+        for options in self.win_options:
+            first = self.board_dict[options[0]]
+            for option in options:
+                win = True if first == self.board_dict[option] != ' ' else False
+                if win == False:
+                    break
+            if win == True:
+                return True
+        return False
+
+    # check if specific player won
+    def Check_Winner(self, letter):
+
+        win = False
+
+        for options in self.win_options:
+            first = letter
+            for option in options:
+                win = True if first == self.board_dict[option] != ' ' else False
+                if win == False:
+                    break
+            if win == True:
+                return True
+        return False
 
     def Draw_Move(self, coordinate, player):
         self.board_dict[coordinate] = player
 
-    def Check_Complate(self):
+    def Check_Complete(self):
         for key in self.board_dict:
             if self.board_dict[key] == ' ':
                 return False
@@ -47,66 +72,100 @@ class Game_Board:
         else:
             return True
 
+    # returns number of free spaces on the board
+    def Free_Spaces(self):
+        free = " "
+        count = 0
+        for key, value in self.board_dict.items():
+            if value == free:
+                count += 1
+        return count
 
-class Player:
-    def __init__(self, current_board):
-        self.board = current_board
-
-    def Check_Legal(self, position):
-        if self.board[position] != ' ':
-            return False
-        else:
-            return True
-
-
-class Ai_Player_Random(Player):
-
-    def Make_Move(self):
-        move = random.randint(1, 9)
-        return move
-
-
-class Human_Player:
-
-    def Pick_Move(self):
-        pass
+    # returns a list of possible moves
+    def Possible_Move(self):
+        free = " "
+        pos_moves = []
+        for key, value in self.board_dict.items():
+            if value == free:
+                pos_moves.append(key)
+        return pos_moves
 
 
 gb = Game_Board()
 End_Criteria = False
-SHAPES = ["X", "O"]
-Current_Player = 0
+Current_Player = "O"
 
 
-def minimax(board, depth, maximisingPLayer):
-    pass
+def minimax(board, player, maximisingPlayer):
+    other_player = "X" if player == "O" else "O"
+
+    if board.Check_Winner(other_player) == True:
+        return [(1*board.Free_Spaces() + 1), None] if maximisingPlayer == False else [(-1*board.Free_Spaces() - 1), None]
+
+    elif board.Check_Complete() == True:
+        return [0, None]
+
+    if maximisingPlayer == True:  # maximising player
+        maxEva = -(math.inf)
+        list_of_moves = board.Possible_Move()
+
+        for move in list_of_moves:
+            board.board_dict[move] = player
+            Eva = minimax(board, other_player, False)
+            if Eva[0] > maxEva:
+                best_move = move
+            maxEva = max(Eva[0], maxEva)
+            board.board_dict[move] = " "
+        return [maxEva, best_move]
+    else:  # minimising player
+        minEva = (math.inf)
+        list_of_moves = board.Possible_Move()
+
+        for move in list_of_moves:
+            board.board_dict[move] = player
+            Eva = minimax(board, other_player, True)
+            if Eva[0] < minEva:
+                best_move = move
+            minEva = min(Eva[0], minEva)
+            board.board_dict[move] = " "
+        return [minEva, best_move]
+
+
+def random_move():
+    move = random.randint(1, 9)
+    return move
 
 
 # main loop
 while End_Criteria == False:
-    This_Turn = int(
-        input(f"{SHAPES[Current_Player]} Current_Player's turn: "))
     Legal = False
+    if Current_Player == "X":
+        This_Turn = int(
+            input(f"{Current_Player} Player's turn: "))
 
-    while Legal == False:
+        while Legal == False:
 
-        if gb.Check_Legal(This_Turn) == False:
-            This_Turn = int(
-                input(f"Illegal move - {SHAPES[Current_Player]} Current_Player's turn again: "))
-        else:
-            gb.Draw_Move(This_Turn, SHAPES[Current_Player])
-            Legal = True
+            if gb.Check_Legal(This_Turn) == False:
+                This_Turn = int(
+                    input(f"Illegal move - {Current_Player} Player1's turn again: "))
+            else:
+                gb.Draw_Move(This_Turn, Current_Player)
+                Legal = True
+    else:
+        This_Turn = minimax(gb, Current_Player, True)
+        gb.Draw_Move(This_Turn[1], Current_Player)
 
     End_Criteria = gb.Check_For_Win()
     gb.Update()
+    print(' ')
 
     # check if game ends else change players
     if End_Criteria == True:
-        print('Winner: ' + SHAPES[Current_Player] + '!!!')
-    elif gb.Check_Complate() == True:
+        print('Winner: ' + Current_Player + '!!!')
+    elif gb.Check_Complete() == True:
         End_Criteria = True
         print('Draw')
-    elif SHAPES[Current_Player] == "X":
-        Current_Player = 1
+    elif Current_Player == "X":
+        Current_Player = "O"
     else:
-        Current_Player = 0
+        Current_Player = "X"
